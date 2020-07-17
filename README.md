33f8f1fd9d50d9d4bc87f7391444596dd7becc5c
Elene Kavtaradze <39622338+ekavtaradze@users.noreply.github.com>
2020-07-17 02:23:27 -0400

    Added MapReduce Join eqv. in Spark Scala

--- README.md
+++ README.md
@@ -3,11 +3,17 @@
 GROUP: Elene Kavtaradze and Michael Ludwig
 This submission is by Elene Kavtaradze and includes problems 1 and 4.
 
+The whole project is built through SBT
 Question One:
     Run QuestionOne class, as scala
 
 Cal add main, delete app because toDF doesn't work anymore
 
 Question Four:
-    Matrix Creation is done through Java
+    Run QuestionFour class, as scala
+    Matrix Creation is done through Java and stored in /data
+    Scala part of Question four picks files from /data
+    
+    Requirement were not super specific, so question four joins
+    the two datasets, then groups them and sums
     
--- src/main/scala/QuestionFour.scala
+++ src/main/scala/QuestionFour.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 object QuestionFour{
    def main(args: Array[String]) {
@@ -32,15 +32,71 @@ object QuestionFour{
       import spark.sqlContext.implicits._
 
 
+    /* val matrix1RDD= spark.sparkContext.textFile("data/M1.txt").map(line=> (line.split(",")(1),
+       Row(line(1).toInt, "M2", line(0).toInt, line(2).toInt) ))
+     println(matrix1RDD)
+     val matrix1DF = spark.sqlContext.createDataFrame(matrix1RDD).show()*/
+     //  .map(t => Row(t(0).toInt, t(1).toInt, t(2).toInt))
+/*   val matrix2RDD= spark.sparkContext.textFile("data/M2.txt").map(_.split(","))
+       .map(t => Row(t(0).toInt, t(1).toInt, t(2).toInt))*/
 
-     val matrix1RDD= spark.sparkContext.textFile("data/M1.txt").map(_.split(","))
+    /* val matrix1RDD= spark.sparkContext.textFile("data/M1.txt").map(_.split(","))
        .map(t => Row(t(0).toInt, t(1).toInt, t(2).toInt))
      val matrix2RDD= spark.sparkContext.textFile("data/M2.txt").map(_.split(","))
        .map(t => Row(t(0).toInt, t(1).toInt, t(2).toInt))
 
 
-     val matrix1DF = spark.sqlContext.createDataFrame(matrix1RDD, matrixSchema).show()
-     val matrix2DF = spark.sqlContext.createDataFrame(matrix2RDD, matrixSchema).show()
+     val matrix1DF = spark.sqlContext.createDataFrame(matrix1RDD, matrixSchema)
+     val matrix2DF = spark.sqlContext.createDataFrame(matrix2RDD, matrixSchema)
+
+     matrix1DF.show()
+     matrix2DF.show()*/
+
+     //map 1 should look like this
+   //  Key: j       value: (FlagM, i, Mij)
+     //map 1 should look like this
+     //  Key: j         value: (FlagN, k, Njk)
+
+   val matrix1MapSchema = new StructType()
+       .add(StructField("J",IntegerType, true))
+       .add(StructField("Matrix1", StringType, true))
+       .add(StructField("I",IntegerType, true))
+       .add(StructField("Value1",IntegerType, true))
+
+     val matrix2MapSchema = new StructType()
+       .add(StructField("J",IntegerType, true))
+       .add(StructField("Matrix2", StringType, true))
+       .add(StructField("K",IntegerType, true))
+       .add(StructField("Value2",IntegerType, true))
+
+     val M1= spark.sparkContext.textFile("data/M1.txt").map(_.split(","))
+       .map(t => Row(t(1).toInt, "M1", t(0).toInt, t(2).toInt))
+     val M2= spark.sparkContext.textFile("data/M2.txt").map(_.split(","))
+       .map(t => Row(t(0).toInt, "M2", t(1).toInt, t(2).toInt))
+
+     val m1DF = spark.sqlContext.createDataFrame(M1, matrix1MapSchema)
+     val m2DF = spark.sqlContext.createDataFrame(M2, matrix2MapSchema)
+
+     m1DF.show()
+     m2DF.show()
+
+
+
+    val joined =  m1DF.join(m2DF, "J")
+
+     joined.show()
+
+     /*
+     Reduce Function (For a given j)•Separate values from M and N•Produce output (i, k, Mij * Njk  = v)
+      */
+     //val reduce = joined.select($"I",$"J", $"K", $"Value1" * $"Value2")
+    val reduce = joined.select($"I", $"K", $"Value1" * $"Value2")
+    // val r = joined.select($"I", $"K", $"Value1 * Value2".alias("Value"))
+
+  reduce.show()
+
+
+
 
 
   }
